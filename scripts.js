195296f4ac1050a9e04db30cbd619fa2966f1c81
Allegra Anka <allegra.anka@gmail.com>
2019-02-20 13:15:48 -0500

    clean slate

--- scripts.js
+++ scripts.js
@@ -3,15 +3,15 @@ console.log("Loaded");
 const axios = require('axios');
 const qs = require('query-string');
 
-const { DOANE_SB_API_TOKEN, DOANE_PR_API_TOKEN } = process.env;
+const { CLIENT_SB_API_TOKEN, CLIENT_PR_API_TOKEN } = process.env;
 const SB_BASE = 'https://sandbox.tinypass.com/api/v3';
 const PR_BASE = 'https://api.tinypass.com/api/v3';
-const sb_aid = 'ewJNmU7sbo';
-const pr_aid = 'HvKpR8HLhx';
+const sb_aid = '';
+const pr_aid = '';
 
 // get custom fields schema
 async function getCustomFields() {
-    const res = await axios.get(`${SB_BASE}/publisher/customFields/get?api_token=${DOANE_SB_API_TOKEN}&aid=${sb_aid}`);
+    const res = await axios.get(`${SB_BASE}/publisher/customFields/get?api_token=${CLIENT_SB_API_TOKEN}&aid=${sb_aid}`);
     let cf_data = JSON.parse(res.data.data);
     // console.log(res.data.data);
     console.log(cf_data);
@@ -19,13 +19,13 @@ async function getCustomFields() {
 }
 getCustomFields();
 
-// edit custom fields schema
+// edit custom fields schema ???
 
 // update, push custom fields schema
 async function updateCustomFields(cf_object) {
     const cf_data = JSON.stringify(cf_object);
     const reqBody = {
-        api_token: DOANE_PR_API_TOKEN,
+        api_token: CLIENT_PR_API_TOKEN,
         aid: pr_aid,
         custom_fields: cf_data
     };
